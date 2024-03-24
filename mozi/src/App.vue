22dde939caf03df29e3f68ae4e6354e93dc1fc8c
Peter <bsze2bagpet@vasvari.org>
2024-03-24 16:42:42 +0100

    .

--- mozi/src/App.vue
+++ mozi/src/App.vue
@@ -33,24 +33,27 @@ const incrises = () =>
 
 <template>
   <div class="d-flex justify-content-center fw-bold my-2 fs-2 ">
-      <p>Filmszemle</p>
+      <p>Filmajánló</p>
   </div>
  <div class="container" >
   <div class="row">
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
       <div class="col" v-for="f in films">
-        <div class="card text-white bg-dark">
+        <div class="card text-white bg-dark border-warning mb-3">
           <img :src="'src/assets/images/filmhib.jpg'" class="card-img-top w-50 mx-2 my-2 px-4 py-4" alt="...">
           <div class="card-body lh-lg">
             <h5 class="card-title">{{ f.title }}</h5>
             <p class="card-text">{{ f.desc }}</p>
           </div>
+          <div class="card-footer">
+            <small class="text-dark fw-bold bg-warning p-1 rounded">Jegyár: 2100FT  </small>
+          </div>
         </div>
       </div>    
-          <button type="button" class="btn btn-danger w-75 text-center fw-bold my-2 ms-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <button type="button" class="btn btn-danger w-100 text-center fw-bold mb-2 " data-bs-toggle="modal" data-bs-target="#exampleModal">
             Szavazatok
           </button>
-          <div class="modal fade text-dark bg-secondary" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade text-dark" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-xl">
               <div class="modal-content">
                 <div class="modal-header">
@@ -58,7 +61,7 @@ const incrises = () =>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body text-back">
-                  Star Wars: Egy új remény <button class="btn btn-warning my-2 mx-2" @click="incrisesw">+</button> 
+                  Star Wars: Egy új remény <button class="btn btn-warning my-1 mx-1" @click="incrisesw">+</button> 
                   <span class="fw-bold">Összes szavazat : {{ votessw }}</span>
                 </div>
                 <div class="modal-body">
